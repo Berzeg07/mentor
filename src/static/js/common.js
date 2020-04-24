a14121995293a0d7f158d7688d084f00ce2b030c
Aslan <berezg86@gmail.com>
2020-04-24 23:18:41 +0300

    24.04.20

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-    alert(1);
+$(document).ready(function() {
+    $('.tab-list .tab-list__btn').click(function() {
+        $('.tab-list__btn').removeClass('is-active');
+        $(this).addClass('is-active');
+        var tab = $(this).attr('tabLink');
+        $('.tab-content').not(tab).css({
+            'display': 'none'
+        });
+        $(tab).fadeIn(400);
+    });
+    $('.tab-list .tab-list__btn:last').click();
 });
